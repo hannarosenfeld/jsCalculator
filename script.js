1df66ef087240f858f1875a71aeb21904423693b
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-16 14:17:48 -0400

    Fix spacing.

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ $(document).ready(function() {
                 //console.log(result)
             } else if ($(this).hasClass('number')) {
                 console.log('number')
-                workingOperation += $(this).text()
+                workingOperation += ' ' + $(this).text()
+                display.innerHTML = workingOperation
             } else if ($(this).hasClass('operand')) {
                 let lastChar = workingOperation.slice(-1);
                 if (lastChar === '+' || lastChar === '*' || lastChar === '/') {
                     workingOperation += ''
                 } else {
-                    workingOperation += ' ' + $(this).text() + ' '
+                    workingOperation += ' ' + $(this).text()
                 }
             }
             secondaryDisplay.innerHTML = workingOperation
