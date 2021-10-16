3c8c1031cf9dca34476908031b3e2e6ec815922f
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-16 11:09:56 -0400

    Refactor. Get addition multiplication and division to be clicked only once.

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ $(document).ready(function() {
     $('button').click(
         function(){
             display.innerHTML = $(this).text()
-            workingOperation += $(this).text()
+
             if ($(this).text()  === 'AC') {
                 return clear()
-            } else if ($(this).text() === '-') {
-                display.innerHTML = $(this).text()
             } else if ($(this).text() === '=') {
-                workingOperation = workingOperation.substring(0, workingOperation.length - 1)
+                console.log(workingOperation)
                 let result = (eval(workingOperation))
                 display.innerHTML = result
-                console.log(result)
+                //console.log(result)
+            } else if ($(this).hasClass('number')) {
+                console.log('number')
+                workingOperation += $(this).text()
+            } else if ($(this).hasClass('operand')) {
+                let lastChar = workingOperation.slice(-1);
+                if (lastChar === '+' || lastChar === '*' || lastChar === '/') {
+                    workingOperation += ''
+                } else {
+                    workingOperation += $(this).text()
+                }
             }
             secondaryDisplay.innerHTML = workingOperation
         }
